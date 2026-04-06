d328bc2acd514fa516d38917bae96b8be1340933
Suyash Rupnawar <suyashcomp512@gmail.com>
2026-04-06 15:11:31 +0530

    Delete service directory

--- service/tests/factories.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def ProductFactory():
-    return {
-        "id": random.randint(1, 1000),
-        "name": "Product" + str(random.randint(1, 100)),
-        "category": "General",
-        "available": True
-    }
