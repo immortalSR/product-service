d2e5d7d177423ce86356bb5ca40f160e19502a15
Suyash Rupnawar <suyashcomp512@gmail.com>
2026-04-06 15:16:35 +0530

    Add initial test cases for route functions

---
+++ tests/test_routes.py
@@ -0,0 +1,20 @@
+def test_read():
+    assert True
+
+def test_update():
+    assert True
+
+def test_delete():
+    assert True
+
+def test_list_all():
+    assert True
+
+def test_list_by_name():
+    assert True
+
+def test_list_by_category():
+    assert True
+
+def test_list_by_availability():
+    assert True
